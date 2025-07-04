1b77159736c22d30ba601a2733b3ed028dfd7b3a
“github-j1an9ty” <chtienyang@hotmail.com>
2025-07-04 17:34:32 +0800

    日常同步

--- 103_0/FreeModbus/port/FreeRTOS/portserial_m.c
+++ 103_0/FreeModbus/port/FreeRTOS/portserial_m.c
@@ -1,3 +1,276 @@
+#include "port.h"
+
+/* ----------------------- Modbus includes ----------------------------------*/
+#include "mb.h"
+#include "mbport.h"
+#define USING_UART2
+
+#if MB_MASTER_RTU_ENABLED > 0 || MB_MASTER_ASCII_ENABLED > 0
+/* ----------------------- Static variables ---------------------------------*/
+static volatile uint8_t rx_buff[FIFO_SIZE_MAX];
+static Serial_fifo Master_serial_rx_fifo;
+/* software simulation serial transmit IRQ handler thread stack */
+/* software simulation serial transmit IRQ handler thread */
+//static TaskHandle_t thread_serial_soft_trans_irq = NULL;
+/* serial event */
+static EventGroupHandle_t event_serial = NULL;
+/* modbus master serial device */
+static UART_HandleTypeDef *serial;
+
+/* ----------------------- Defines ------------------------------------------*/
+/* serial transmit event */
+#define EVENT_SERIAL_TRANS_START    (1<<0)
+
+/* ----------------------- static functions ---------------------------------*/
+static void prvvUARTTxReadyISR(void);
+static void prvvUARTRxISR(void);
+static void serial_soft_trans_irq(void *parameter);
+//static void Master_TxCpltCallback(struct __UART_HandleTypeDef *huart);
+void Master_RxCpltCallback(struct __UART_HandleTypeDef *huart);
+static int stm32_getc(void);
+static int stm32_putc(CHAR c);
+
+/* ----------------------- Start implementation -----------------------------*/
+BOOL xMBMasterPortSerialInit(UCHAR ucPORT, ULONG ulBaudRate, UCHAR ucDataBits,
+        eMBParity eParity)
+{
+    /**
+     * set 485 mode receive and transmit control IO
+     * @note MODBUS_MASTER_RT_CONTROL_PIN_INDEX need be defined by user
+     */
+
+    /* set serial name */
+  if (ucPORT == 1) 
+  {
+#if defined(USING_UART1)
+    extern UART_HandleTypeDef huart1;
+    serial = &huart1;
+//    //printf("Master using uart1!\r\n");
+#endif
+  } 
+  else if (ucPORT == 2) 
+  {
+#if defined(USING_UART2)
+    extern UART_HandleTypeDef huart2;
+    serial = &huart2;
+//    //printf("Master using uart2!\r\n");
+#endif
+  }
+   else if (ucPORT == 3) 
+   {
+#if defined(USING_UART3)
+    extern UART_HandleTypeDef huart3;
+    serial = &huart3;
+//    //printf("Master using uart3!\r\n");
+#endif
+  }
+    /* set serial configure parameter */
+	serial->Init.BaudRate = ulBaudRate;
+	serial->Init.StopBits = UART_STOPBITS_1;
+    switch(eParity){
+    case MB_PAR_NONE: {
+	    serial->Init.WordLength = UART_WORDLENGTH_8B;
+	    serial->Init.Parity = UART_PARITY_NONE;
+        break;
+    }
+    case MB_PAR_ODD: {
+	    serial->Init.WordLength = UART_WORDLENGTH_9B;
+	    serial->Init.Parity = UART_PARITY_ODD;
+        break;
+    }
+    case MB_PAR_EVEN: {
+	    serial->Init.WordLength = UART_WORDLENGTH_9B;
+	    serial->Init.Parity = UART_PARITY_EVEN;
+        break;
+    }
+    }
+	if (HAL_UART_Init(serial) != HAL_OK) 
+	{
+		Error_Handler();
+	}
+	__HAL_UART_DISABLE_IT(serial, UART_IT_RXNE);
+	__HAL_UART_DISABLE_IT(serial, UART_IT_TC);
+	/*registe recieve callback*/
+//	HAL_UART_RegisterCallback(serial, HAL_UART_RX_COMPLETE_CB_ID, Master_RxCpltCallback);
+	/* software initialize */
+	Master_serial_rx_fifo.buffer = rx_buff;
+	Master_serial_rx_fifo.get_index = 0;
+	Master_serial_rx_fifo.put_index = 0;
+	/* software initialize */
+
+	/* 创建串口事件组 */
+	event_serial = xEventGroupCreate();
+	if (NULL != event_serial)
+	{
+			//printf("主任务创建串口事件组成功! event_serial=%d\r\n",(int)event_serial);
+	}
+	else 
+	{
+			//printf("主任务创建串口事件组失败!\r\n");
+	}
+
+	/* 创建串口软传输中断处理任务 */
+	BaseType_t xReturn = pdPASS;
+	xReturn = xTaskCreate((TaskFunction_t)serial_soft_trans_irq, /* 任务入口函数 */
+												(const char *)"master trans",          /* 任务名称 */
+												(uint16_t)128,                         /* 任务堆栈大小 */
+												(void *)NULL,                          /* 任务入口函数参数 */
+												(UBaseType_t)12,                       /* 任务优先级 */
+												NULL);                                 /* 任务句柄 */
+	if (xReturn == pdPASS) 
+	{
+			//printf("创建主传输任务成功\r\n");
+	}
+	else 
+	{
+			//printf("创建主传输任务失败!\r\n");
+			return FALSE;
+	}
+
+	return TRUE;
+
+}
+
+/**
+ * 功能：启用或禁用Modbus主机串口的接收和发送功能
+ * 参数：
+ *   xRxEnable - 是否启用接收中断
+ *   xTxEnable - 是否启用发送功能
+ */
+void vMBMasterPortSerialEnable(BOOL xRxEnable, BOOL xTxEnable)
+{
+    /* 清除接收和发送标志位，确保状态机初始状态一致 */
+    __HAL_UART_CLEAR_FLAG(serial, UART_FLAG_RXNE);
+    __HAL_UART_CLEAR_FLAG(serial, UART_FLAG_TC);
+    
+    if (xRxEnable == pdTRUE) 
+    {
+        /* 启用串口接收中断 */
+        __HAL_UART_ENABLE_IT(serial, UART_IT_RXNE);
+    } 
+    else 
+    {
+        /* 禁用串口接收中断 */
+        __HAL_UART_DISABLE_IT(serial, UART_IT_RXNE);
+    }
+    
+    if (xTxEnable == pdTRUE)
+    {
+        /* 触发串口发送事件，启动数据传输 */
+        xEventGroupSetBits(event_serial, EVENT_SERIAL_TRANS_START);
+    }
+    else
+    {
+        /* 清除发送事件标志，停止数据传输 */
+        xEventGroupClearBits(event_serial, EVENT_SERIAL_TRANS_START);
+    }
+}
+
+void vMBMasterPortClose(void)
+{
+	__HAL_UART_DISABLE(serial);
+}
+
+BOOL xMBMasterPortSerialPutByte(CHAR ucByte)
+{
+    stm32_putc(ucByte);
+    return TRUE;
+}
+
+BOOL xMBMasterPortSerialGetByte(CHAR * pucByte)
+{
+    Get_from_fifo(&Master_serial_rx_fifo, (uint8_t *)pucByte, 1);
+    return TRUE;
+}
+
+/*
+ * Create an interrupt handler for the transmit buffer empty interrupt
+ * (or an equivalent) for your target processor. This function should then
+ * call pxMBFrameCBTransmitterEmpty( ) which tells the protocol stack that
+ * a new character can be sent. The protocol stack will then call
+ * xMBPortSerialPutByte( ) to send the character.
+ */
+void prvvUARTTxReadyISR(void)
+{
+    pxMBMasterFrameCBTransmitterEmpty();
+}
+
+/*
+ * Create an interrupt handler for the receive interrupt for your target
+ * processor. This function should then call pxMBFrameCBByteReceived( ). The
+ * protocol stack will then call xMBPortSerialGetByte( ) to retrieve the
+ * character.
+ */
+void prvvUARTRxISR(void)
+{
+    pxMBMasterFrameCBByteReceived();
+}
+
+/**
+ * 软件模拟串口发送中断处理函数
+ *
+ * @param parameter 任务参数
+ */
+static void serial_soft_trans_irq(void* parameter) {
+    while (1) 
+    {
+        /* 等待串口发送启动事件 */
+        xEventGroupWaitBits(event_serial,             /* 事件组句柄 */
+                            EVENT_SERIAL_TRANS_START, /* 等待设置的事件位 */
+                            pdFALSE,        /* 不自动清除等待的事件位 */
+                            pdFALSE,        /* 任意一个事件位被设置即可唤醒 */
+                            portMAX_DELAY); /* 无限等待，直到事件发生 */
+        
+        /* 事件发生后，执行Modbus发送回调函数 */
+        prvvUARTTxReadyISR();
+    }
+}
+
+/**
+ * @brief  Rx Transfer completed callbacks.
+ * @param  huart  Pointer to a UART_HandleTypeDef structure that contains
+ *                the configuration information for the specified UART module.
+ * @retval None
+ */
+void Master_RxCpltCallback(struct __UART_HandleTypeDef *huart) 
+{
+	int ch = -1;
+	/*UART RX·????D??μ÷ó?￡?2￠??è?ò???êy?Y*/
+	while (1) 
+	{
+		ch = stm32_getc();
+		if (ch == -1)
+		{
+			break;
+		}
+		Put_in_fifo(&Master_serial_rx_fifo, (uint8_t *)&ch, 1);
+	}
+	prvvUARTRxISR();
+}
+/*UART·￠?íò???êy?Y*/
+static int stm32_putc(CHAR c) 
+{
+	serial->Instance->DR = c;
+	while (!(serial->Instance->SR & UART_FLAG_TC));
+	return TRUE;
+}
+/*UART?óê?ò???êy?Y*/
+static int stm32_getc(void) 
+{
+	int ch;
+	ch = -1;
+	if (serial->Instance->SR & UART_FLAG_RXNE) 
+	{
+		ch = serial->Instance->DR & 0xff;
+	}
+	return ch;
+}
+
+#endif
+
+
+
+#if 0
 /*
  * FreeModbus Libary: RT-Thread Port
  * Copyright (C) 2013 Armink <armink.ztl@gmail.com>
@@ -28,6 +301,7 @@
 #include "bsp.h"
 
 #if MB_MASTER_RTU_ENABLED > 0 || MB_MASTER_ASCII_ENABLED > 0
+
 /* ----------------------- Static variables ---------------------------------*/
 ALIGN(RT_ALIGN_SIZE)
 /* software simulation serial transmit IRQ handler thread stack */
@@ -222,4 +496,5 @@ static rt_err_t serial_rx_ind(rt_device_t dev, rt_size_t size) {
     return RT_EOK;
 }
 
+#endif
 #endif
